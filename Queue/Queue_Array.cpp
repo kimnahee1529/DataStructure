4dde6c57ac754e3d3f61fd0c1b9add6a9f3b496f
kimnahee1529 <65105035+kimnahee1529@users.noreply.github.com>
2020-11-15 17:04:23 +0900

    Create Queue_Array.cpp

---
+++ Queue/Queue_Array.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+#define SIZE 10000
+#define INF 99999999
+
+int queue[SIZE];
+int front = 0;
+int rear = 0;
+
+void push(int data) {
+	if (rear >= SIZE) {
+		cout<<"overflow!"<<endl;
+		return;
+	}
+	queue[rear++] = data;
+}
+int pop() {
+	if (front == rear) {
+		cout<<"underflow"<<endl;
+		return -INF;
+	}
+	return queue[front++];
+}
+void show() {
+	cout<<"Queue's front"<<endl;
+	for (int i = front; i < rear; i++) {
+		cout<<queue[i]<<endl;
+	}
+	cout<<"Queue's rear"<<endl;
+}
+int main() {
+	push(7);
+	push(5);
+	push(4);
+	pop();
+	push(6);
+	pop();
+	show();
+	return 0;
+}
